0b86bb1cefb28800fa1f58e6557a9a3c816239c8
Patrick Bennett <patrickisgreat@gmail.com>
2019-04-24 15:01:25 -0400

    Update composer so we can install fork via Packagist Update Service Provider to be extensible and register a singleton

--- src/Artisaninweb/SoapWrapper/ServiceProvider.php
+++ src/Artisaninweb/SoapWrapper/ServiceProvider.php
@@ -23,14 +23,15 @@ class ServiceProvider extends LaravelServiceProvider
    */
   public function register()
   {
-    $soapWrapper = new SoapWrapper();
+    $this->soapWrapper = new SoapWrapper();
 
     if (is_array($this->app['config']['soapwrapper'])) {
-      $soapWrapper->addByArray($this->app['config']['soapwrapper']);
+      $this->soapWrapper->addByArray($this->app['config']['soapwrapper']);
     }
+    $soapWrapper = $this->soapWrapper;
 
-    $this->app->bindIf(SoapWrapper::class, function () use ($soapWrapper) {
-      return $soapWrapper;
+    $this->app->singleton(SoapWrapper::class, function () use ($soapWrapper) {
+      return $this->soapWrapper;
     });
   }
 }
